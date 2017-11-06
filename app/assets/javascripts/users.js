fbe4c55ead618cf6fcbe04a24a54eecc981d080c
Jignesh Satam <jigneshsatam@Jigneshs-MacBook-Pro.local>
2017-11-07 00:17:00 +0530

    Build clock

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -3,18 +3,40 @@
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 
 $(document).ready(function(){
-  GetClock();
-  setInterval(GetClock,1000);
+  setDate();
+  setInterval(GetClock, 1200);
 });
 tday = new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
 tmonth = new Array("January","February","March","April","May","June","July","August","September","October","November","December");
 
-function GetClock(){
+function setDate(){
   var d = new Date();
   var nday = d.getDay(),nmonth=d.getMonth(),ndate=d.getDate(),nyear=d.getFullYear();
-  var nhour = d.getHours(),nmin=d.getMinutes(),nsec=d.getSeconds();
-  if(nmin<=9) nmin="0"+nmin
-  if(nsec<=9) nsec="0"+nsec;
+  $("#day p").text(tday[nday]);
+  $("#month p").text(ndate + " " + tmonth[nmonth]);
+  $("#year p").text(nyear);
+}
 
+function GetClock(){
+  var d = new Date();
+  var nhour = d.getHours(),nmin=d.getMinutes(),nsec=d.getSeconds();
+  if(nhour<=9) nhour="0"+nhour;
+  if(nmin<=9) nmin="0"+nmin;
+  if(nsec<=9) nsec="0"+nsec;
+  if ($("#hh p").text() != nhour){
+    $("#hh").addClass("change");
+  }
+  if ($("#mm p").text() != nmin){
+    $("#mm").addClass("change");
+  }
+  if ($("#ss p").text() != nsec){
+    $("#ss").addClass("change");
+  }
+  $(".ui.horizontal.segments.time .change").transition('slide down');
+  $("#hh p").html(nhour);
+  $("#mm p").html(nmin);
+  $("#ss p").html(nsec);
+  $(".ui.horizontal.segments.time  .change").transition('slide up');
+  $(".change").removeClass("change");
   // document.getElementById('clockbox').innerHTML=""+tday[nday]+", "+tmonth[nmonth]+" "+ndate+", "+nyear+" "+nhour+":"+nmin+":"+nsec+"";
 }
