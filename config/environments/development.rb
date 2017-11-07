9e741d16730b50ae342b5664238be20a775680e2
JigneshSatam <jigneshsatam@gmail.com>
2017-11-07 17:00:27 +0530

    Added smtp settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,4 +53,14 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.smtp_settings = {
+    :address =>  "smtp.sendgrid.net",
+    :port =>  587,
+    :domain =>  "time-tracker-exercise.herokuapp.com",
+    :user_name =>  "app80532757@heroku.com",
+    :password =>  "9ywfgujf3471",
+    :authentication => :plain,
+    :enable_starttls_auto =>  true
+  }
 end
