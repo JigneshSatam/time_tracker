aa5da4c8b6196223628d90033b01848afa77620a
JigneshSatam <jigneshsatam@gmail.com>
2017-11-07 17:11:34 +0530

    Added smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,11 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { host: ENV['APP_HOST'] }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
     :address =>  "smtp.sendgrid.net",
     :port =>  587,
